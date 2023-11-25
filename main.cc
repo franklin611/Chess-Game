f132bd8e35eed077dbb8377b708b1c0a154c45a8
franklin611 <franklin.ramirez6111@gmail.com>
2023-11-25 15:44:59 -0500

    - Coding Session Changes

--- main.cc
+++ main.cc
@@ -18,16 +18,12 @@ int convertToInt(char c) {
 }
 
 
-
-
 int main() {
     string cmd;
-    // ChessBoard cb;
     Xwindow xw;
 
     while (cin >> cmd) {
         // I would outpu to the user 1. What the game in a cool design 2. The rules of the game 
-        // 
         
         if(cmd == "game") {
             //To setup a new game, we take two input playerWhite playerBlack.
@@ -49,51 +45,88 @@ int main() {
                 int level = player2[9] // The number
                 Player playerWhite = Computer{1,level};
             }
-            string cmd2;
-
-
-            //For chessboard, we now have our players.
-            // Need to keep track of the input whether they are playerWhite or playerBlack
-            // As well as if its a computer's turn or not
             chessBoard{playerWhite, playerBlack} cb;
-            while(cin >> cmd2) { // This would be the word move.
+
+            string cmd2;
+            while(cin >> cmd2) { 
+                // 
+
                 if(cb.getTurn()) { //If passes means playerWhite turn
-                    if (player1 == "human") {
+                    if (cmd2 == "move" && player1 == "human") {
                         string start, end;
                         cin >> start >> end;
-                        // Make Player1 do a move - TO DO - As well as check if the move is valid, do we need to do it here
-                        // TO DO - As well determine the pieces on start and where it wants to go so that if it is necessary pawn promotion, get another input
+
+                        int x = convertToInt(start.substr(0,1));
+                        int y = (int) start.substr(1);
+                        Vec coordinate1 = Vec{x,y};
+
+                        int x = convertToInt(end.substr(0,1));
+                        int y = (int) end.substr(1);
+                        Vec coordinate2 = Vec{x,y};
+
+                        if(cb.makeHumanMove(coordinate1, coordinate2)) {
+                            // Valid Move
+                            // Check if Pawn moved to either end of board depending on black or white. Dont really need to check if black end or white end cuz until this?
+                            // Point, we are assuming they are valid moves
+
+                            if(cb.upgradePawn(coordinate2)) {
+                                char newPiece;
+                                cin >> newPiece;
+                                cb.setupWithChar(newPiece, coordinate2);
+                            }
+
+                        } else {
+                            // Invalid Move, try again!
+                        }
+                        // In makeHumanMove, we already check if move is valid. All we do inmain is just pass it.
+                        // Is valid will check and determine based on what piece is located at start whether valid or not.  
+
+                    } else if(cmd2 == "move" && player1 == "computer") {
+                        // Make Computer Move
+                        Vec end = cb.makeComputerMove(&playerWhite);
+                        if(cb.upgradePawn(end)){
+                            cb.setupWithChar(newPiece, end);
+                        }
                     } else if(cmd2 == "resign") {
                         //Player1 now has to lose 
-                        player1.setCheckmate(true); // Set player's checkmate field to be true
-                        if(end()) { // If this is true, this means the game can end. But I dont think so since the game already should be over since player1.setCheckmate is set to true
-                            // What even needs to be done here? -TO DO
-                        }
+                        // Create a chessboard function called forfeit that passes a bool. This function calls updateWhite or updateBlack depending on who won
+                        // We pass who lost
 
-
-                    } else {
-                        // Maker Player1 do a move as a Computer
-                    }
+                        cb.forfeit(1);
+                        // This function will update the white and black score and restart the chesboard.                   
+                    } 
                 } else {
-                    if (player2 == "human") {
+                    if (cmd2 == "move" && player2 == "human") {
                         string start, end;
                         cin >> start >> end;
-                        // Make Player2 do a move - TO DO
-                    } else if(cmd2 == "resign") {
-                        player2.setCheckmate(true);
-                        if(end()) {
-                            // TO DO
+
+                        string start, end;
+                        cin >> start >> end;
+
+                        int x = convertToInt(start.substr(0,1));
+                        int y = (int) start.substr(1);
+                        Vec coordinate1 = Vec{x,y};
+
+                        int x = convertToInt(end.substr(0,1));
+                        int y = (int) end.substr(1);
+                        Vec coordinate2 = Vec{x,y};
+
+                        if(cb.makeHumanMove(coordinate1, coordinate2)) {
+                            if(cb.upgradePawn(coordinate2)) {
+                                char newPiece;
+                                cin >> newPiece;
+                                cb.setupWithChar(newPiece, coordinate2);
+                            }
+                    } else if (cmd == "move" && player2 == "computer") {
+                        Vec end = cb.makeComputerMove(&playerBlack);
+                        if(cb.upgradePawn(end)){
+                            cb.setupWithChar(newPiece, end);
                         }
-
-                    } else {
-                        // Maker Player2 do a move as a Computer
-                    }
+                    } else if(cmd2 == "resign") {
+                        cb.forfeit(0);
+                    } 
                 }
-                
-
             } // This for loop continuously gets input from the user regarding moves. 
-
-
 
         } else if (cmd == "setup") { // Enters setup mode TO DO : Where do we get user input for players?
             string cmd2, coord, colour;
@@ -114,45 +147,28 @@ int main() {
                         break;
                     
                     }
-                    cb.setupWithChar(piece, coordinate);
-                    _ 
-                    ' '
-
-                    // Can't think of anything else here
+                    cb.setupWithChar(piece, coordinate); 
                 } else if (cmd2 == "-") {
                     cin >> coord;
                     int x = convertToInt(piece.substr(0,1));
                     int y = (int) piece.substr(1);
-                    // How do we want to remove pieces. I guess we can signify by passing empty to Piece?
-                    // We need to place an empty piece there.
-                    // Even if we get the empty piece at this vec Coord, how do we actually put this piece on the chessboard. 
-                    // I think we have to be able to pass a _ and a " " to setup to set up a white or black piece. 
+                    // Remove a piece on a board by placing an empty piece on that coordinate
                     Vec coordinate = Vec{x,y};
                     cb.setupWithPiece(getEmptyPiece(coordinate), coordinate);
-
-
-                    // cb.setup('', Vec{x,y}); Might not even need setup, We could use the empty board to crossreference? TO DO
-
                 } else if (cmd2 == "=") { 
                     // From my understanding, "makes it colors turn to go next" means when the game start, it is their turn
                     while (cin >> colour) {
                         if(colour == "black") {
-                            // Set black turn to true.
-                            cb.setTurn(0); // Set turn to 0, indicating black?  TO DO
+                            // Chiara confirmed this is how it works. 
+                            cb.setTurn(0); 
                             break;
-
-
                         } else if (colour == "white") {
                             cb.setTurn(1);
                             break;
-
                         } else {
-                            // They have inputted invalid input
-                            // Prompt them for input again. 
                             cout << "Invalid input, please try again." << endl;
                         }
-                    }
-                    
+                    } 
                 } else { // User has entered "done" to leave setup mode. 
                 // 1. Need to check the current state of the board to see if it is valid. If not,  make them re-set up?  TO DO ASKED PIAZZA
                 // Piazza said we had creative freedom. I personally think it is better to have a resetBoard. 
@@ -163,21 +179,21 @@ int main() {
                     if(cb.ValidBoard()) { //
                         break;
                     } else {
+                        cout << "Invalid Board Setup. Please re-setup again." << endl;
+                        cout << "You had an extra blah blah" << endl; // Create logic for determining where it went wrong. We can do this in main or Chessboard -TO DO
                         // output invalid board and explain why it was
                         // Resetboard aned start from the very beginnign. Instef of very beginnign of "Welcome"
                         // Just to the point wher tehy were setting up the board again. But we have to start witha fresh board
 
+                        // No break
+                        cb.restartGame();
                     }
                 }
-
             }
         } else { // Invalid Input
             cout << "Invalid Input, try Again" << endl;
             // At the beginning of the game, do we want to output to the user how to the game works/rules!!!! TO DO
-
         }
-    
-
     }
 }
 
