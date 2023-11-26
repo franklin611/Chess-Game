7baabf9d7c7c7a50d1cee78d75fa57ba186a7294
franklin611 <franklin.ramirez6111@gmail.com>
2023-11-26 10:55:55 -0500

    Updated to unique_ptr

--- main.cc
+++ main.cc
@@ -17,39 +17,78 @@ int convertToInt(char c) {
     }
 }
 
+void outputRules() {
+    string art =
+    "+----------------------------------------------------+\n"
+    "|                                                    |\n"
+    "|                    Chess Game                      |\n"
+    "|                                                    |\n"
+    "|          By: Chiara, Franklin, Helena              |\n"
+    "|                                                    |\n"
+    "+----------------------------------------------------+\n";  
+    cout << art << endl; 
+    cout << "Welcome to our chess game. Please follow the rules and commands strictly.\n";
+    cout << "Commands:\n";
+    cout << "  - game white-player black-player: Starts a new game. 'white-player' and 'black-player' can be 'human' or 'computer[1-4]'.\n";
+    cout << "  - resign: Concede the game to your opponent.\n";
+    cout << "  - move <start> <end>: Make a move from <start> to <end>. For castling, use king's initial and final positions.\n";
+    cout << "  - move <start> <end> <promotion>: Pawn promotion move.\n";
+    cout << "  - move: Computer player will make a move.\n\n";
+
+    cout << "Setup Commands (only available when a game is not running):\n";
+    cout << "  - setup: Enter setup mode.\n";
+    cout << "  - + <char> <coords>: Add piece at the specified coordinates.\n";
+    cout << "  - - <coords>: Remove piece at the specified coordinates.\n";
+    cout << "  - = <colour>: Set the next turn to the specified colour.\n";
+    cout << "  - done: Exit setup mode, if all setup conditions are met.\n\n";
+
+    cout << "In setup mode, ensure the following conditions are met before exiting:\n";
+    cout << "  - One white king and one black king must be on the board.\n";
+    cout << "  - No pawns are on the first or last rows of the board.\n";
+    cout << "  - Neither king is in check.\n\n";
+
+    cout << "Please input your commands:\n";
+}
+
 
 int main() {
     string cmd;
-    Xwindow xw;
+    // Xwindow xw;
+    bool usedSetup = false;
+    chessBoard cb = chessboard{};
+    outputRules();
 
-    while (cin >> cmd) {
-        // I would outpu to the user 1. What the game in a cool design 2. The rules of the game 
+    while (cin >> cmd) {   
+         
         
         if(cmd == "game") {
+            // Once the user has entered game, they should not be able to exit the below while loop until the game is over. 
             //To setup a new game, we take two input playerWhite playerBlack.
             String player1, player2;
             cin >> player1 >> player2;
 
-            // There must be a more efficient way to this i wont even lie
+            unique_ptr<Player> playerWhite, playerBlack;
+
             if(player1 == "human") {
-                Player playerWhite = Human{1};
+                playerWhite = make_unique<Human>(1)
             } else {
-                // computer[1]
-                int level = player1[9] // The number
-                Player playerWhite = Computer{1,level};
+                int level = stoi(player1.substr(9));
+                playerWhite = make_unique<Human>(1, level);
             }
             if(player2 == "human") {
-                Player playerBlack = Human{0};
+                playerBlack = make_unique<Human>(0)
             } else {
-                // computer[1]
-                int level = player2[9] // The number
-                Player playerWhite = Computer{1,level};
+                int level = stoi(player2.substr(9)); // The number
+                playerBlack = make_unique<Human>(0, level);
             }
-            chessBoard{playerWhite, playerBlack} cb;
 
+            if(usedSetup) cb.setupPlayers(playerWhite, playerBlack);
+            else cb = ChessBoard{playerWhite, playerBlack, usedSetup};
+            // Use a default board
+        
             string cmd2;
-            while(cin >> cmd2) { 
-                // 
+            // Should not be able to exit this while loop until game is over and ctrl d is run. 
+            while(cin >> cmd2) {
 
                 if(cb.getTurn()) { //If passes means playerWhite turn
                     if (cmd2 == "move" && player1 == "human") {
@@ -68,12 +107,14 @@ int main() {
                             // Valid Move
                             // Check if Pawn moved to either end of board depending on black or white. Dont really need to check if black end or white end cuz until this?
                             // Point, we are assuming they are valid moves
+                            // what if they do move e2 e3 K, need to handle
 
                             if(cb.upgradePawn(coordinate2)) {
                                 char newPiece;
                                 cin >> newPiece;
                                 cb.setupWithChar(newPiece, coordinate2);
-                            }
+                            } else cin.ignore();
+                            // Ignore the single next character input by the user. 
 
                         } else {
                             // Invalid Move, try again!
@@ -116,7 +157,7 @@ int main() {
                                 char newPiece;
                                 cin >> newPiece;
                                 cb.setupWithChar(newPiece, coordinate2);
-                            }
+                            } else cin.ignore();
                     } else if (cmd == "move" && player2 == "computer") {
                         Vec end = cb.makeComputerMove(0, level);
                         if(cb.upgradePawn(end)){
@@ -126,12 +167,19 @@ int main() {
                         cb.forfeit(0);
                     } 
                 }
-            } // This for loop continuously gets input from the user regarding moves. 
+                // 1. Check if game is over
+                if(isEnd()) {
+                    
+                }
 
-        } else if (cmd == "setup") { // Enters setup mode TO DO : Where do we get user input for players?
+            }
+          // This for loop continuously gets input from the user regarding moves. 
+
+        } else if (cmd == "setup") { // Input from players is solved by making the user enter game .
             string cmd2, coord, colour;
             char piece;
-            Chessboard cb;
+            // I am pretty sure I can just pass usedSetup but that will mean we need to default construct in ChessBoard.h so no diffeence. 
+            Chessboard cb = ChessBoard{null, null, usedSetup};
 
             cin >> cmd2;
             while(cin >> cmd2) {
@@ -194,6 +242,7 @@ int main() {
             cout << "Invalid Input, try Again" << endl;
             // At the beginning of the game, do we want to output to the user how to the game works/rules!!!! TO DO
         }
+    }
     }
 }
 
