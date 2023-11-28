5854c85f4ace2684efb44e675e3b697e68607a08
franklin611 <franklin.ramirez6111@gmail.com>
2023-11-28 13:55:39 -0500

    FIxed bishop functions

--- Bishop.cc
+++ Bishop.cc
@@ -26,16 +26,16 @@ void Bishop::resetMoves() {
 void Bishop::resetMoves() {
 	legalMoves.clear();
 	// determine all legal moves 
-	for (vector<Piece> vec : game->getGameBoard()){
-		for (Piece p : vec){
-			vector<Vec> moves = bishopMoves(); 
-			for (Vec end: moves){
-				if (!willCheck(coordinate, end)){
-					legalMoves.push_back(end);
-				};
-			} 
-		}
-	}
+	// for (vector<Piece> vec : game->getGameBoard()){
+	// 	for (Piece p : vec){
+	// 		vector<Vec> moves = bishopMoves(); 
+	// 		for (Vec end: moves){
+	// 			if (!willCheck(coordinate, end)){
+	// 				legalMoves.push_back(end);
+	// 			};
+	// 		} 
+	// 	}
+	// }
 }
 
 // Just all possible moves
@@ -43,71 +43,50 @@ void Bishop::resetMoves() {
 // I know you said just possibleMoves, but if I create a getter method that takes the type and checks if a piecee exists there
 // That if that piece is of the same team, we cant go there, but if an enemy team, bishop cna go there
 // And that is the last move bishop can make and break the for loop
-vector<Vec> Bishop::bishopMoves() {
-    vector<Vec> moves;
+void Bishop::getPossibleMoves(vector<vector<shared_ptr<Piece>>> gb) {
 
     // 4 for loops
     // To top right, to top left, to bottom right, to bottom left. Just keep continuously checking that we are withing bounds
     
-    // // To top left Diagonal
-    // for (int i = 1; i < 7; ++i) {
-    //     if((coordinate.getX() - i >= 0)  && (coordinate.getY() - i >= 0)) {
-    //         moves.push_back(Vec{coordinate.getX() - i, coordinate.getY() - i});
-    //     } else {
-    //         break;
-    //     }
-    // }
-
-    // // To top right Diagonal
-    // for (int i = 1; i < 7; ++i) {
-    //     if((coordinate.getX() + i <= 7)  && (coordinate.getY() - i >= 0)) {
-    //         moves.push_back(Vec{coordinate.getX() + i, coordinate.getY() - i});
-    //     } else {
-    //         break;
-    //     }
-    // }
-
-    // // To bottom left Diagonal
-    // for (int i = 1; i < 7; ++i) {
-    //     if((coordinate.getX() - i >= 0)  && (coordinate.getY() + i <= 7)) {
-    //         moves.push_back(Vec{coordinate.getX() - i, coordinate.getY() + i});
-    //     } else {
-    //         break; 
-    //     }
-    // }
-
-    // // To bottom right Diagonal
-    // for (int i = 1; i < 7; ++i) {
-    //     if((coordinate.getX() + i >= 0)  && (coordinate.getY() + i >= 0)) {
-    //         moves.push_back(Vec{coordinate.getX() + i, coordinate.getY() + i});
-    //     } else {
-    //         break;
-    //     }
-    // }
-
-    // To top right, to top left, to bottom right, to bottom left. Just keep continuously checking that we are withing bounds
+    // To top left Diagonal
     for (int i = 1; i < 7; ++i) {
-        // To top left Diagonal
-        if((coordinate.getX() - i >= 0)  && (coordinate.getY() - i >= 0)) {
-            moves.push_back(Vec{coordinate.getX() - i, coordinate.getY() - i});
-        } 
-        // To top right Diagonal
-        if((coordinate.getX() + i <= 7)  && (coordinate.getY() - i >= 0)) {
-            moves.push_back(Vec{coordinate.getX() + i, coordinate.getY() - i});
-        } 
-        // To bottom left Diagonal
-        if((coordinate.getX() - i >= 0)  && (coordinate.getY() + i <= 7)) {
-            moves.push_back(Vec{coordinate.getX() - i, coordinate.getY() + i});
-        }
-        // To bottom right Diagonal
-        if((coordinate.getX() + i >= 0)  && (coordinate.getY() + i >= 0)) {
-            moves.push_back(Vec{coordinate.getX() + i, coordinate.getY() + i});
+        Vec coord = Vec{coordinate.getX() - i, coordinate.getY() - i};
+
+        if(inBounds(Vec) && isEmptyPiece(pieceAt(gb,coord))) {
+            possibleMoves.push_back(coord);
+        } else if (inBounds(coord) && piecAt(gb, coord)->getTeam() != getTeam()) {
+            possibleMoves.push_back(coord);
         }
     }
 
-    return moves;
+    // To top right Diagonal
+    for (int i = 1; i < 7; ++i) {
+        Vec coord = Vec{coordinate.getX() + i, coordinate.getY() -i};
+         if(inBounds(Vec) && isEmptyPiece(pieceAt(gb,coord))) {
+            possibleMoves.push_back(coord);
+        } else if (inBounds(coord) && piecAt(gb, coord)->getTeam() != getTeam()) {
+            possibleMoves.push_back(coord);
+        }
+    }
 
-// bool getColour();
-// void Piece::getColour() return white; // Returns the boolean value of whether it is true or not
+    // To bottom left Diagonal
+    for (int i = 1; i < 7; ++i) {
+        Vec coord = Vec{coordinate.getX() - i, coordinate.getY() + i};
+         if(inBounds(Vec) && isEmptyPiece(pieceAt(gb,coord))) {
+            possibleMoves.push_back(coord);
+        } else if (inBounds(coord) && piecAt(gb, coord)->getTeam() != getTeam()) {
+            possibleMoves.push_back(coord);
+        }
+    }
+
+    // To bottom right Diagonal
+    for (int i = 1; i < 7; ++i) {
+        Vec coord = Vec{coordinate.getX() + i, coordinate.getY() + i};
+         if(inBounds(Vec) && isEmptyPiece(pieceAt(gb,coord))) {
+            possibleMoves.push_back(coord);
+        } else if (inBounds(coord) && piecAt(gb, coord)->getTeam() != getTeam()) {
+            possibleMoves.push_back(coord);
+        }
+    }
 
 }
