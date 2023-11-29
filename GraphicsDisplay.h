e2706a8d5d6755fdb02e48f7e6cdeea3066015c4
Franklin Ramirez <121467771+franklin611@users.noreply.github.com>
2023-11-28 23:36:31 -0500

    Revert "Chessboard constructor fix"

--- GraphicsDisplay.h
+++
@@ -1,7 +0,0 @@
-#include "DisplayObserver.h"
-#include <vector>
-class GraphicsDisplay: public DisplayObserver{
-
-    public: 
-        void notify(Vec start, char typeStart, Vec end, char typeEnd) override;
-};
--- Player.cc
+++
@@ -1,9 +0,0 @@
-#include "Player.h"
-
-Player::Player(bool colour, unique_ptr<Observer> cb) : colour{colour}, cb{make_unique<Observer>()} {}
-
-void Player::notify(Vec start, Vec end) {
-    vector<Vec> newMove = {start, end};
-    legalMoves.emplace_back(newMove);
-}
-
