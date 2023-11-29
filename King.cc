ef4443df35e427664fc0e64b90fa5918943b8826
Franklin Ramirez <franklin.ramirez6111@gmail.com>
2023-11-28 19:04:32 -0500

    initial code

--- King.cc
+++ King.cc
@@ -1,7 +1,34 @@
 #include "King.h"
-
 #include "Player.h"
+#include "Piece.h"
+
+King::King() : Piece{}, moved{false} {}
 
 void King::hasMoved(){
     moved = true;
 }
+
+void::getMOved() {
+    return moved;
+}
+
+void King::getPossibleMoves(vector<vector<shared_ptr<Piece>>> gb) {
+    Vec move;
+
+    Vec moveUp;
+    Vec moveLeft;
+    Vec moveRight;
+    Vec moveDown;
+    
+}
+
+
+
+
+    public: 
+        void resetMoves() override; 
+        King();
+        vector<Vec> kingMoves();
+        bool getMoved();
+        void hasMoved();
+};
