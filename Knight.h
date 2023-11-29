d848d4efb0a2f26f3c02109a1359b8e315f52dcf
Franklin Ramirez <franklin.ramirez6111@gmail.com>
2023-11-28 20:45:13 -0500

    Fixed Knight

--- Knight.h
+++ Knight.h
@@ -2,8 +2,7 @@
 
 class Knight : public Piece {
     public: 
-        void resetMoves() override; 
         Knight();
-        vector<Vec> knightMoves();
+        void getPossibleMoves(vector<vector<shared_ptr<Piece>>> gb) override;
 };
 
