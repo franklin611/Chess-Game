e112d8bbb53ad4b9955354989c9ba1eb33cb0f68
Franklin <franklin.ramirez6111@gmail.com>
2023-12-03 17:52:17 -0500

    TextDisplay Comments

--- TextDisplay.cc
+++ TextDisplay.cc
@@ -3,7 +3,9 @@
 #include "ChessBoard.h"
 using namespace std;
 
+// Default constructor - Creates the empty board
 TextDisplay::TextDisplay() {
+    // Creates the default board with '_' and ' ' 
     for (size_t i = 0; i <= 7; ++i) {
         vector<char> row;
         for (size_t j = 0; j <= 7; ++j) {
@@ -17,26 +19,28 @@ TextDisplay::TextDisplay() {
     }
 }
 
+// change thedisplay's character at start, change thedisplay's character at end
 void TextDisplay::notifyMoves(Vec start, char typeStart, Vec end, char typeEnd, string check) { // notify should be only printing the moves
     str_check = check;
-    // cout << "CHECK STRING: " << check << endl; 
-    // change thedisplay's character at start, change thedisplay's character at end
     theDisplay[start.getY()][start.getX()] = typeStart;
     theDisplay[end.getY()][end.getX()] = typeEnd;
 }
 
+// Changes the TextDisplay's Char at Vec start
 void TextDisplay::notify(Vec start, char typeStart) {
     theDisplay[start.getY()][start.getX()] = typeStart;
 }
 
-void TextDisplay::notify(bool white) { // call this inside of setup as well as when making any move
-// Everything is inverted!
+void TextDisplay::notify(bool white) { 
+    // call this inside of setup as well as when making any move
     if (white) {colour = "Black";}
     else {colour = "White";}
 }
 
 ostream &operator<<(ostream &out, const TextDisplay &td) {
     int num = 8;
+    // Outer loop prints backwards, from max to 0 because our actual gameboard is inverted from an actual chessboard
+    // In our gameboard, Vec{0,0} is the Right white rook from the White King
     for (int i = td.theDisplay.size() - 1; i >= 0; --i) {
         out << to_string(num) + ' ';
         for (size_t j = 0; j < td.theDisplay.size(); ++j) {
@@ -48,6 +52,7 @@ ostream &operator<<(ostream &out, const TextDisplay &td) {
     out << endl;
     out << "  abcdefgh" << endl;
     out << endl;
+    // Outputs The turn and if a player is in check
     if (td.str_check.length() > 0) out << td.str_check << " is in check." << endl; // empty string is true, so if not empty string output
     out << "It's " << td.colour << "'s turn" << endl;
     out << "---------------------" << endl;
