f9fa99fdd9ab291126ff64dc1accf7772c7a77d5
h32xu <h32xu@uwaterloo.ca>
2023-12-03 22:29:59 -0500

    added comments

--- GraphicsDisplay.h
+++ GraphicsDisplay.h
@@ -5,17 +5,21 @@
 #include "Window.h"
 #include <vector>
 #include <string>
-
 #include <memory>
+
+// Graphics Display class which is a subclass of DisplayObserver class
+// Graphics Display also has a pointer to Xwindow
 class GraphicsDisplay: public DisplayObserver{
     unique_ptr<Xwindow> w;
     int dim;
     public:
         GraphicsDisplay();
+        // Overwritten notifyMoves to change the display's character at Vec start, and change the char at Vec end
         void notifyMoves(Vec start, char typeStart, Vec end, char typeEnd, string check) override;
+        // Overwritten notify to change the display's character at a specific coordinate
         void notify(Vec start, char typeStart) override;
         void notify(bool white) override;
-        virtual ~GraphicsDisplay();
+        ~GraphicsDisplay() = default; // default since using unique_ptrs
 };
 
 #endif
